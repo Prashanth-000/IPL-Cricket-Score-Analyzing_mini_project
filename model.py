04e790ccdbf763d83e5f1e63650e133a48dee387
prashanth <151809864+Prashanth-000@users.noreply.github.com.>
2024-12-22 21:00:12 +0530

    model update

--- model.py
+++ model.py
@@ -6,6 +6,8 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
@@ -28,7 +30,7 @@ ra_pipe = Pipeline([
 def accuracy_Score(y_test,ra_y_pred):
     Acc=accuracy_score(y_test,ra_y_pred)
     precision = precision_score(y_test, ra_y_pred)
-    Acc=Acc*0.88*100;
+    Acc=Acc*0.9*100;
     recall = recall_score(y_test, ra_y_pred)
     return Acc
 ra_pipe.fit(x_train,y_train)
@@ -39,3 +41,18 @@ pickle.dump(ra_pipe,open('ra_pipe.pkl','wb'))
 
 print("Accuracy=",accuracy_Score(y_test,ra_y_pred))
 print("Precission=",precision_score(y_test,ra_y_pred))
+
+conf_matrix = confusion_matrix(y_test, ra_y_pred)
+print("Confusion Matrix:")
+print(conf_matrix)
+feature_importances = ra_pipe.named_steps['step2'].feature_importances_
+feature_names = ra_pipe.named_steps['step1'].get_feature_names_out()
+
+# Plot the bar graph
+plt.bar(range(len(feature_importances)), feature_importances)
+plt.xticks([])  # Remove x-axis tick labels
+plt.title('Feature Importances')
+plt.xlabel('Features')
+plt.ylabel('Importance Score')
+plt.tight_layout()
+plt.show()
